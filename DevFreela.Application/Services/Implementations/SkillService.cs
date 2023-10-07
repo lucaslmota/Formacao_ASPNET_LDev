4d58a2dc22493907f9ec2f709299ee2b9ebb392f
lucaslmota <lucaslmota0431@gmail.com>
2023-10-07 15:16:41 -0300

    F22-30: camada aplication-parte5

---
+++ DevFreela.Application/Services/Implementations/SkillService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class SkillService : ISkillService
+    {
+        public List<SkillViewModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
