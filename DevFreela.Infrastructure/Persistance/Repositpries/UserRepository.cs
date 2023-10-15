4630bfa18177ff38be9d1a575c74a7339e1a003e
lucaslmota <lucaslmota0431@gmail.com>
2023-10-15 15:50:47 -0300

    F-32 - 61 add a UserRepository refact

--- DevFreela.Infrastructure/Persistance/Repositpries/UserRepository.cs
+++ DevFreela.Infrastructure/Persistance/Repositpries/UserRepository.cs
@@ -2,12 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevFreela.Core.Entities;
 using DevFreela.Core.Repositories;
 
 namespace DevFreela.Infrastructure.Persistance.Repositpries
 {
     public class UserRepository : IUserRepository
     {
+        public Task AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<User> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUserByEmailAndPasswordAsync(string email, string passwordHash)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
