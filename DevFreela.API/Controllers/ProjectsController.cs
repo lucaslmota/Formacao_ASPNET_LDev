2d3f7dcdebb0c801cd071fe0330e71dba4679b9d
lucaslmota <lucaslmota0431@gmail.com>
2023-10-19 14:40:45 -0300

    feat: ajuste controller Projects

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,54 +1,112 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevFreela.Application.Commands.CreatComment;
+using DevFreela.Application.Commands.CreateProject;
+using DevFreela.Application.Commands.FinishProject;
+using DevFreela.Application.Commands.StartProject;
+using DevFreela.Application.Commands.UpdateProject;
 using DevFreela.Application.InputModels;
-using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.Queries.GetAllProjects;
+using DevFreela.Application.Queries.GetProjectById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/projects")]
+    [Authorize]
     public class ProjectsController : ControllerBase
     {
-        private readonly IProjectService _projectService;
-        public ProjectsController(IProjectService projectService)
+        private readonly IMediator _mediator;
+        public ProjectsController(IMediator mediator)
         {
-            _projectService = projectService;
+            _mediator = mediator;
         }
 
+        //api/projects?query=net core
         [HttpGet]
-        public IActionResult Get(string query)
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> Get(string query)
         {
-            var projects = _projectService.GetAll(query);
+            var getAllProjectsQuery = new GetAllProjectsQuery(query);
+            var projects = await _mediator.Send(getAllProjectsQuery);
             return Ok(projects);
         }
 
+        // api/projects/2
         [HttpGet("{id}")]
-        public IActionResult GetById(int id)
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> GetById(int id)
         {
-            var project = _projectService.GetById(id);
-
+            var project = new GetProjectByIdQuery(id);
             if (project == null)
             {
                 return NotFound();
             }
-            return Ok();
+
+            await _mediator.Send(project);
+            return Ok(project);
 
         }
 
         [HttpPost]
-        public IActionResult Post([FromBody] NewProjectInpuModel newProjectInpuModel)
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Post([FromBody] CreateProjectCommand command)
         {
-            return Ok();
+            if (!ModelState.IsValid)
+            {
+                var messages = ModelState
+                    .SelectMany(ms => ms.Value.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+                return BadRequest(messages);
+            }
+
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, command);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] UpdateInputModel updateInputModel)
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Put(int id, [FromBody] UpdateProjectCommand command)
         {
-            return Ok();
+            if (command.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            await _mediator.Send(command);
+
+            return NoContent();
         }
 
+        [HttpPost("{id}/comments")]
+        [Authorize(Roles = "client, freelancer")]
+        public async Task<IActionResult> PostComment(int id, [FromBody] CreateCommentCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
 
+        [HttpPut("{id}/start")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Start(int id)
+        {
+            var command = new StartProjectCommand(id);
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        [Authorize(Roles = "client")]
+        public async Task<IActionResult> Finish(int id, [FromBody] FinishProjectCommand command)
+        {
+            command.IdProject = id;
+            var result = await _mediator.Send(command);
+            if (!result)
+                return BadRequest("O pagamento não pode ser processado");
+
+            return Accepted();
+        }
     }
 }
