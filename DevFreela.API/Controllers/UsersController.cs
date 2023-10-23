a83b1d17ef5438bab34269ca3c2cf82200b18611
lucaslmota <lucaslmota0431@gmail.com>
2023-10-23 11:09:26 -0300

    F36-74-Implementando o Login

---
+++ DevFreela.API/Controllers/UsersController.cs
@@ -0,0 +1,68 @@
+using DevFreela.Application.Commands.CreatUser;
+using DevFreela.Application.Commands.LoginUser;
+using DevFreela.Application.Queries.GetUserById;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/users")]
+    [Authorize]
+    public class UsersController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UsersController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = new GetUserByIdQuery(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _mediator.Send(user);
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Post([FromBody] CreateUserCommand command)
+        {
+            if (!ModelState.IsValid)
+            {
+                var messages = ModelState
+                    .SelectMany(ms => ms.Value.Errors)
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+
+                return BadRequest(messages);
+            }
+
+            var id = await _mediator.Send(command);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, command);
+        }
+
+        [HttpPut("login")]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
+        {
+            var loginUserViewModel = await _mediator.Send(command);
+
+            if (loginUserViewModel == null)
+            {
+                return BadRequest();
+            }
+            return Ok(loginUserViewModel);
+        }
+    }
+}
--- DevFreela.Application/Commands/LoginUser/LoginUserCommandHandler.cs
+++ DevFreela.Application/Commands/LoginUser/LoginUserCommandHandler.cs
@@ -1,17 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DevFreela.Application.ViewModels;
+using DevFreela.Core.Repositories;
+using DevFreela.Core.Service;
 using MediatR;
 
 namespace DevFreela.Application.Commands.LoginUser
 {
     public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, LoginUserViewModel>
     {
-        public Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        private readonly IAuthService _authService;
+        private readonly IUserRepository _userRepository;
+
+        public LoginUserCommandHandler(IAuthService authService, IUserRepository userRepository)
         {
-            throw new NotImplementedException();
+            _authService = authService;
+            _userRepository = userRepository;
+        }
+        public async Task<LoginUserViewModel> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            //Utilizar o mesmo algoritmo para criar o hash dessa senha
+            var passwordHash = _authService.ComputeSha256Hash(request.Password);
+            //Buscar no mei banco de dados um User que tenha um e-mail e minha senha em formato hash
+            var user = await _userRepository.GetUserByEmailAndPasswordAsync(request.Email, passwordHash); ;
+            //Se não exixtri, erro no login
+            if (user == null)
+            {
+                return null;
+            }
+            //Se existir, gera o token usando os dados do susuário
+            var token = _authService.GenerateJwtToken(user.Email, user.Role);
+
+            return new LoginUserViewModel(user.Email, token);
         }
     }
 }
