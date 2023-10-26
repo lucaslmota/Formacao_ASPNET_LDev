03841bbbdc12262a70c54daf5a6d8afee96f650b
lucaslmota <lucaslmota0431@gmail.com>
2023-10-26 18:57:44 -0300

    #F42-83-Escrevendo o primeiro teste unitario

---
+++ DevFreela.UnitTest/Core/Entities/ProjectTest.cs
@@ -0,0 +1,33 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTest.Core.Entities
+{
+    public class ProjectTest
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()// Testar se o projeto inicia
+        {
+            var project = new Project("Nome de test", "Descirção de teste",1,2, 1000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
